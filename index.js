9f66b9733fc5ed6bc691211ddf31ccf5891f4d3e
biosoluwa <favbiose@gmail.com>
2026-01-25 04:37:49 +0100

    typography

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ document.addEventListener('click', function(e){
     }else if(e.target.classList.contains('remove')){
         handleRemoveBtnClick(e.target.id)
     }else if(e.target.id === 'complete-order'){
+        document.getElementById('order').classList.remove('showOnPage')
        modal.style.display = 'inline'
     } else if(e.target.id === 'pay-btn'){
         submitForm()   
@@ -78,11 +79,11 @@ function renderMenu(){
     menuArray.forEach(function(menu){ 
               menuItems +=   `<div class="menu-items">
                                     <div class="image-and-desc">
-                                        <p class="emoji"> ${menu.emoji} </p>
+                                      <span class="emoji"> ${menu.emoji}</span>
                                         <div class="menu-item-description">
                                             <h2>${menu.name}</h2>
                                             <p class="grey-text">${menu.ingredients}</p>
-                                            <p><strong>$${menu.price}</strong></p>
+                                            <p>$${menu.price}</p>
                                         </div>
                                     </div>
                                     <img src="images/add-btn.png" class="add-btn" id="${menu.id}">

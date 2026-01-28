9205897231e0c74dfe2ce3d3f192ef09b2faba33
biosoluwa <favbiose@gmail.com>
2026-01-28 14:33:09 +0100

    fixed remove btn

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ import {menuArray} from "./data.js";
 let orderArray = []
 const cardForm = document.getElementById('card-form')
 
-
 const modal = document.getElementById('card-details-modal')
 const thanksDiv = document.getElementById('thanks')
 
 
-
+// event listeners
 document.addEventListener('click', function(e){
     if(e.target.classList.contains('add-btn')){
     handleAddBtnClick(e.target.id)
@@ -20,15 +19,16 @@ document.addEventListener('click', function(e){
        modal.style.display = 'inline'
     } 
 })
-
+// event listener for the form 
 cardForm.addEventListener('submit', function(e){
     e.preventDefault()
     submitForm() 
     orderArray = []
-
     thanksDiv.style.display = 'flex'
 })
 
+
+// when user clicks on pay button 
 function submitForm(){
     let nameInput = document.getElementById('userName')
     let cardInput = document.getElementById('card-number')
@@ -45,41 +45,59 @@ function submitForm(){
     thanksDiv.innerHTML = `<p>Thanks ${name}! Your order is on it's way</p>`   
 }
 
+
+// when user clicks on the add(+) button beside the menu 
 function handleAddBtnClick(menuId){
     const targetObj = menuArray.filter(function(menu){
         return (Number(menuId) === menu.id)
     })[0]
-    orderArray.push(targetObj)
+
+    if(orderArray.includes(targetObj)){
+        targetObj.count++
+    }else{
+            targetObj.count = 1
+            orderArray.push(targetObj)
+    }
     thanksDiv.style.display = 'none'
     renderOrder()
 }
 
+// when user clicks on the remove text beside the order items 
 function handleRemoveBtnClick(menuId){
-    const targetObj = menuArray.filter(function(menu){
-        return (Number(menuId) === menu.id)
-    })[0]
-    orderArray.pop(targetObj)
-    renderOrder()
+     
+       orderArray = orderArray.filter(function(item){
+            if(item && item.id === Number(menuId) ){
+               return item.count --
+            }else{
+                return Number(menuId) !== item.id
+            }
+       })
+
+       orderArray.forEach(function(){
+
+       })
+       renderOrder()
 }
 
+// show the order details on the page 
 function renderOrder(){
+     let orderHtml = ''
     document.getElementById('order').classList.add('showOnPage')
 
-    let orderHtml = ''
-
     orderArray.forEach(function(item){
+      item.totalItemPrice = item.price * item.count
     orderHtml += `
         <div class="order-items">
             <div class="remove-item">
-                 <p class="big-text">${item.name}</p>
-                 <p class="remove">remove</p>
+                 <p class="big-text">${item.name} (x${item.count})</p>
+                 <p class="remove" id="${item.id}">remove</p>
             </div>
-            <p>$${item.price}</p>
+            <p>$${item.totalItemPrice}</p>
         </div>
     ` })
 
   const totalOrder = orderArray.reduce(function(total, currentItem){
-        return total + currentItem.price
+        return total + currentItem.totalItemPrice
     }, 0)
     orderHtml += ` <div class="total-order">
                       <p class="big-text">Total price:</p>
@@ -88,6 +106,9 @@ function renderOrder(){
     document.getElementById('order-details').innerHTML = orderHtml  
 }
 
+
+
+// to render the menu on the page (beer, pizza, hamburger)
 function renderMenu(){
     let menuItems = ''
     menuArray.forEach(function(menu){ 
